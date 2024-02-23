efaa80a29cbedee1cd25ed3e5c1821a5c138ffb6
nyebe-14 <ramirezian037@gmail.com>
2024-02-23 17:54:31 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@ using namespace std;
 
 struct Queue
 {
+  // Address
 	Queue* next = NULL;
+  // Data
 	int number = 0;
 	string name = "";
 };
@@ -36,55 +38,58 @@ int main() {
 		case 1:
 			cout << "Add\n";
 
-			if (!counter)
+			if (!counter) // If counter is 0
 			{
-				head = new Queue;
-				head->next = NULL;
+				head = new Queue; // head ay gagawa ng bagong queue
+				head->next = NULL; // in head the next will contain null
 
+        // add data
 				cout << "Enter Int: ";
 				cin >> head->number;
 
 				cout << "Enter Name: ";
 				cin >> head->name;
 
-				tail = head;
+				tail = head; // tail is pointing to head
 
 			}
-			else {
-				temp = new Queue;
-				temp->next = tail;
+			else // else counter is not 0
+      {
+				temp = new Queue; // temp is making new Queue
+				temp->next = tail; // in temp there is next where contains tail
 
+        // add data
 				cout << "Enter Int: ";
 				cin >> temp->number;
 
 				cout << "Enter Name: ";
 				cin >> temp->name;
 
-				tail = temp;
+				tail = temp; // tail will be updated
 			}
 
-			counter++;
+			counter++; // add counter
 
 			break;
 		case 2:
 			cout << "Traverse\n\n";
 
-			temp = tail;
+			temp = tail; // temp will point to the tail
 
-			if (temp)
+			if (temp) // if temp is not empty
 			{
 				do
 				{
-					cout << temp->number << ". " << temp->name << "\n";
+					cout << temp->number << ". " << temp->name << "\n"; // display the data
 
-					if (temp->next == NULL)
+					if (temp->next == NULL) // if the next is empty break the loop
 						break;
 
-					temp = temp->next;
+					temp = temp->next; // replace temp with the next from temp
 				}
 				while (true);
 			}
-			else
+			else // else empty
 			{
 				cout << "No List Yet";
 			}
@@ -95,28 +100,30 @@ int main() {
 		case 3:
 			cout << "Delete";
 
+      // accept what to delete
 			cout << "What to Delete: ";
 			cin >> toDelete;
 
+      // temp will point tail
 			temp = tail;
 
 			do
 			{
-				if (toDelete == temp->name) {
-					if (prev != NULL)
+				if (toDelete == temp->name) { // if toDelete is match with the temps name
+					if (prev != NULL) // if the prev is not empty
 						prev->next = temp->next;
 
 					delete temp;
+			    counter--;
 					break;
 				}
 
-				prev = temp;
-				temp = temp->next;
+        if (temp->next == NULL) // if the next is empty break the loop
+						break;
+
+				prev = temp; // prev will point temp
+				temp = temp->next; // temp will point the temp next
 			} while (true);
-
-			counter--;
-
-
 			break;
 		default:
 			break;
